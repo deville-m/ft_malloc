c88d613b566f24b1ac5017fc297a26f4cc2efd48
Matthew Deville <mdeville@student.42.fr>
2019-11-06 13:21:21 +0100

    Fix NULL pointer bugs

--- srcs/free.c
+++ srcs/free.c
@@ -6,7 +6,7 @@
 /*   By: mdeville <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 16:56:01 by mdeville          #+#    #+#             */
-/*   Updated: 2019/11/05 19:24:37 by mdeville         ###   ########.fr       */
+/*   Updated: 2019/11/06 13:17:58 by mdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,29 +20,6 @@
 
 t_header	*g_free[3] = {NULL, NULL, NULL};
 size_t		g_page_size = 4096;
-
-int			find_lst(t_header *head, t_header *lst)
-{
-	t_header *walk;
-
-	if (!lst)
-		return (0);
-	walk = lst->next;
-	while (walk != lst)
-	{
-		if (walk == head)
-			return (1);
-		walk = walk->next;
-	}
-	return (0);
-}
-
-int			fuck_macos_read_posix(t_header *head)
-{
-	return (find_lst(head, g_free[0]) ||
-			find_lst(head, g_free[1]) ||
-			find_lst(head, g_free[2]));
-}
 
 void		restore(t_header *chunk, t_header **free_lst)
 {
@@ -67,9 +44,9 @@ void		free(void *p)
 	t_header *chunk;
 	size_t real_size;
 
+	if (!p)
+		return;
 	chunk = p - sizeof(t_header);
-	if (!fuck_macos_read_posix(chunk))
-		return (void)write(2, "Invalid pointer beeing freed\n", 29);
 	if (chunk->free)
 		return (void)write(2, "Double free\n", 12);
 	chunk->free = false;
--- srcs/realloc.c
+++ srcs/realloc.c
@@ -6,7 +6,7 @@
 /*   By: mdeville <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/05 18:06:52 by mdeville          #+#    #+#             */
-/*   Updated: 2019/11/05 19:24:18 by mdeville         ###   ########.fr       */
+/*   Updated: 2019/11/06 13:19:22 by mdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,15 @@ void	*realloc(void *ptr, size_t size)
 {
 	t_header	*chunk;
 	void		*p;
+	size_t		final;
 
+	if (!ptr)
+		return (malloc(size));
 	if (!(p = malloc(size)))
 		return (NULL);
 	chunk = ptr - sizeof(t_header);
-	if (fuck_macos_read_posix(chunk))
-		ft_memcpy(p, ptr, chunk->size);
+	final = (size < chunk->size) ? size : chunk->size;
+	ft_memcpy(p, ptr, final);
 	free(ptr);
 	return (p);
 }
